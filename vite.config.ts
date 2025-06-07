41aaf36ea5f06d64a321156945e9cea1c1e6857f
luongtrihoang <luongtrihoang1994@gmail.com>
2025-06-07 22:31:59 +0700

    Update vercel config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/vue/', // Replace with your repository name
+  base: '/', // Root path for Vercel deployment
 })
